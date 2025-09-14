5f0c0e4ad4348817805695003d209d9b2c5c6aed
P3dr006 <pedrovitorcesario@gmail.com>
2025-09-14 11:21:57 -0300

    ajeitando relatorios para informar onde arquivo foi criado

--- gerenciamento_arquivo/relatorios.py
+++ gerenciamento_arquivo/relatorios.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 
-import os
+pasta_inicial =  Path('exercicios/exercicios-python-1/gerenciamento_arquivo')
 
-pasta_inicial =  Path('exrcicios')
+nova_pasta = pasta_inicial/'dados'
 
-nova_pasta = pasta_inicial/'arquivos'
-
-nova_pasta.mkdir(exist_ok=True)
+nova_pasta.mkdir(parents=True,exist_ok=True)
 
 print(f'pasta criada em :{nova_pasta}')
