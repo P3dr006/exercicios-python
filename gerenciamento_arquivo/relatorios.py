47bd6bbd88d2f7016801c8b518a3f3787046361c
P3dr006 <pedrovitorcesario@gmail.com>
2025-09-14 11:42:10 -0300

    reajustei para que ele leia e retorne caso o arquivo ja tenha sido criado.

--- gerenciamento_arquivo/relatorios.py
+++ gerenciamento_arquivo/relatorios.py
@@ -1,9 +1,20 @@
 from pathlib import Path
 
-pasta_inicial =  Path('exercicios/exercicios-python-1/gerenciamento_arquivo')
+pasta_inicial =  Path('gerenciamento_arquivo/')
 
 nova_pasta = pasta_inicial/'dados'
 
 nova_pasta.mkdir(parents=True,exist_ok=True)
 
-print(f'pasta criada em :{nova_pasta}')
+
+
+arquivo = nova_pasta/'arquivo.txt'
+arquivo.touch(exist_ok=True)
+if not arquivo.exists():
+    print('arquivo nao existe')
+    print(f'pasta criada em :{nova_pasta}')
+
+elif arquivo.exists():
+    print('arquivo ja existe')
+
+
